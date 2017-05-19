5ee021a9979e6dacbb2bbccb3c4ac79b76a75133
debugging-sky <aakash89qwerty@gmail.com>
2017-05-19 20:45:17 +0530

    Add files via upload

---
+++ Birthday Cake Candles.cpp
@@ -0,0 +1,84 @@
+/*
+https://www.hackerrank.com/challenges/birthday-cake-candles
+
+
+Colleen is turning years old! She has candles of various heights on her cake, and candle has height . Because the taller candles tower over the shorter ones, Colleen can only blow out the tallest candles.
+
+Given the for each individual candle, find and print the number of candles she can successfully blow out.
+
+Input Format
+
+The first line contains a single integer, , denoting the number of candles on the cake.
+The second line contains space-separated integers, where each integer describes the height of candle .
+
+Constraints
+
+Output Format
+
+Print the number of candles Colleen blows out on a new line.
+
+Sample Input 0
+
+4
+3 2 1 3
+
+Sample Output 0
+
+2
+
+Explanation 0
+
+We have one candle of height , one candle of height , and two candles of height . Colleen only blows out the tallest candles, meaning the candles where . Because there are such candles, we print on a new line.
+
+*/
+
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    int max=0;
+    int count=0;
+    int height;
+    
+    for(int i=0;i<n;i++){
+     cin>>height;
+        
+       if(height==max)  // if height is tallest
+        {count++;}      // increase the num of candles that can be blown
+        
+        else if(max<height){     // else reset and start again
+              count=1;
+            max=height;
+        }
+        
+    }
+    
+    cout<<count;
+    
+       return 0;
+}
